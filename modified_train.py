13c914cab9b36764789a52303c19aeab6ea69fcc
ppriyank <priyankpathak50@gmail.com>
2019-11-30 16:11:03 -0500

    modifications

--- modified_train.py
+++ modified_train.py
@@ -64,57 +64,6 @@ val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=True
 
 
 
-# def true_box(images, boxes, labels, difficulties,name=""):
-# 	resize = transforms.Resize((300, 300))
-# 	to_tensor = transforms.ToTensor()
-# 	normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-# 	                                 std=[0.229, 0.224, 0.225])
-# 	det_boxes = boxes.to('cpu')
-# 	# Transform to original image dimensions
-# 	# original_dims = torch.FloatTensor([4032, 3024, 4032, 3024]).unsqueeze(0)
-# 	original_dims = torch.FloatTensor([300, 300, 300, 300]).unsqueeze(0)
-# 	det_boxes = det_boxes * original_dims
-# 	# Decode class integer labels
-# 	det_labels = [rev_label_map[l] for l in labels.to('cpu').tolist()]
-# 	# If no objects found, the detected labels will be set to ['0.'], i.e. ['background'] in SSD300.detect_objects() in model.py
-# 	if det_labels == ['background']:
-# 	    image_to_show = images
-#     # Annotate
-# 	images_array = images.to('cpu').permute(1,2,0).numpy()
-# 	temp = (images_array - np.min(images_array)) * 255 // (np.max(images_array) - np.min(images_array)) 
-# 	annotated_image = Image.fromarray(np.array(temp).astype(np.uint8))
-# 	original_image = annotated_image
-# 	original_image.save(name + "_temp2.jpg", "JPEG")
-# 	draw = ImageDraw.Draw(annotated_image)
-# 	font = ImageFont.truetype("./arial.ttf", 15)
-# 	label_color_map
-# 	for i in range(det_boxes.size(0)):
-# 	    # Boxes
-# 	    box_location = det_boxes[i].tolist()
-# 	    draw.rectangle(xy=box_location, outline=label_color_map[det_labels[i]])
-# 	    draw.rectangle(xy=[l + 1. for l in box_location], outline=label_color_map[
-# 	        det_labels[i]])  # a second rectangle at an offset of 1 pixel to increase line thickness
-# 	    # draw.rectangle(xy=[l + 2. for l in box_location], outline=label_color_map[
-# 	    #     det_labels[i]])  # a third rectangle at an offset of 1 pixel to increase line thickness
-# 	    # draw.rectangle(xy=[l + 3. for l in box_location], outline=label_color_map[
-# 	    #     det_labels[i]])  # a fourth rectangle at an offset of 1 pixel to increase line thickness
-# 	    # Text
-# 	    text_size = font.getsize(det_labels[i].upper())
-# 	    text_location = [box_location[0] + 2., box_location[1] - text_size[1]]
-# 	    textbox_location = [box_location[0], box_location[1] - text_size[1], box_location[0] + text_size[0] + 4.,
-# 	                        box_location[1]]
-# 	    draw.rectangle(xy=textbox_location, fill=label_color_map[det_labels[i]])
-# 	    draw.text(xy=text_location, text=det_labels[i].upper(), fill='white',
-# 	              font=font)
-# 	annotated_image.save(name + "_temp.jpg", "JPEG")
-
-
-# images, boxes, labels, difficulties = next(iter(train_dataset))
-# true_box(images, boxes, labels, difficulties)
-
-
-
-
 from detect import detect
 
 
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Label map
 # voc_labels = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
               # 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
-voc_labels = ('laptop', 'person')
+voc_labels = ('laptop', 'person', 'lights', 'drinks' , 'projector')
 label_map = {k: v + 1 for v, k in enumerate(voc_labels)}
 label_map['background'] = 0
 rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
